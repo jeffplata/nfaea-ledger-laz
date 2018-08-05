777dcaf9a5d894f69f9cf2810f5384157cf424ac
JeffPlata <jeffplata@yahoo.com>
2018-08-05 21:56:41 +0800

    working on visitors

--- visitors.pas
+++ visitors.pas
@@ -6,6 +6,8 @@ interface
 
 uses
   Classes, SysUtils
+  ,tiOPFManager
+  ,tiObject
   ,tiVisitorDB
   ,ledger_bom
   ;
@@ -13,6 +15,8 @@ uses
 type
 
   { TReadPersonsVisitor }
+
+  TReadPersonVisitor = class(TtiVisitorSelect);
 
   TReadPersonsVisitor = Class(TtiVisitorSelect)
   Protected
@@ -22,21 +26,61 @@ type
     Procedure MapRowToObject; override;
   end;
 
-  TSavePersonsVisitor = Class(TtiVisitorUpdate)
+  { TSavePersonVisitor }
+
+  TSavePersonVisitor = Class(TtiVisitorUpdate)
   Protected
     Procedure Init; override;
     Function AcceptVisitor : Boolean; override;
     Procedure SetupParams; override;
   end;
 
+  procedure RegisterVisitors;
+
 
 implementation
+
 
 const
   SQLReadPersons = 'select OID, NAME, DATEJOINED from PERSON';
   SQLCreatePerson = 'insert into PERSON(OID,NAME,DATEJOINED) values (:OID,:NAME,:DATEJOINED)';
   SQLUpdatePerson = 'update PERSON set NAME=:NAME, DATEJOINED=:DATEJOINED where OID=:OID';
   SQLDeletePerson = 'delete from PERSON where OID=:OID';
+
+
+{ TSavePersonVisitor }
+
+procedure TSavePersonVisitor.Init;
+begin
+  Case Visited.ObjectState of
+    posCreate:
+      Query.SQLText:=SQLCreatePerson;
+    posUpdate:
+      Query.SQLText:=SQLUpdatePerson;
+    posDelete:
+      Query.SQLText:=SQLDeletePerson;
+  end;
+end;
+
+function TSavePersonVisitor.AcceptVisitor: Boolean;
+begin
+  Result:=Visited is TPerson;
+  Result:=Result and (Visited.ObjectState in [posCreate,posdelete,posUpdate]);
+end;
+
+procedure TSavePersonVisitor.SetupParams;
+var
+  O : TPerson;
+
+begin
+  O:=TPerson(Visited);
+  O.OID.AssignToTIQuery('OID',Query);
+  if (Visited.ObjectState<>posDelete) then
+  begin
+    Query.ParamAsString['NAME'] := O.Name;
+    Query.ParamAsDateTime['DATEJOINED'] := O.DateJoined;
+  end;
+end;
 
 { TReadPersonsVisitor }
 
@@ -64,11 +108,23 @@ procedure TReadPersonsVisitor.MapRowToObject;
     O.OID.AssignFromTIQuery('OID',Query);
 
     O.Name:= Query.FieldAsString['NAME'];
-    O.DateJoined:= Query.FieldAsString['DATEJOINED'];
-    O.DateJoinedAsString := Query.FieldAsString['DATEJOINED'];
+    O.DateJoined:= Query.FieldAsDateTime['DATEJOINED'];
     O.ObjectState:=posClean;
     TPersonList(Visited).Add(O);
 end;
+
+procedure RegisterVisitors;
+begin
+  with GTIOPFManager do
+  begin
+    RegReadVisitor(TReadPersonsVisitor);
+    RegSaveVisitor(TSavePersonVisitor);
+  end;
+end;
+
+
+initialization
+  RegisterVisitors;
 
 end.
 
